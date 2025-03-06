ffcc48b3236fd4b531ac03a39af7dbb37bcabffb
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-06 14:39:17 +0300

    comments on code

--- State-Binding/ProfileView.swift
+++ State-Binding/ProfileView.swift
@@ -8,29 +8,37 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var toggle: Bool = false
-    @State private var name: String = ""
-    @State private var displayedName: String = "World"
+    @State private var toggle: Bool = false // this toggle is for the darmode function
+    @State private var name: String = "" // empty string to fill the name later
+    @State private var displayedName: String = "World" // the first string displayed is world just to mke look good
     var body: some View {
+        // ZStack to put all the content inside
         ZStack {
-            toggle ? Color.gray.ignoresSafeArea(edges: .all) : Color.white.ignoresSafeArea(edges: .all)
+            // this is where the toglle is used for changing background color
+            toggle ? Color.gray.ignoresSafeArea(edges: .all) : Color.white.ignoresSafeArea(edges: .all) // here to make the safe area the same color
+            // VStack to the content appear beneath each other
             VStack {
+                // the main text shown on the top
                 Text("Profile")
                     .font(.largeTitle)
-                    .foregroundColor(toggle ? .white : .black)
+                    .foregroundColor(toggle ? .white : .black) // this is to change the text color based on the background color
+                
+                // this is where we change the background color
                 Toggle("Background Color", isOn: $toggle)
                     .padding()
                     .toggleStyle(.switch)
                     .foregroundColor(toggle ? .white : .black)
-                
+                Spacer()
+                // here I show a text with name entered
                 Text("Hello, \(displayedName)!")
                     .font(.headline)
                     .padding()
                     .foregroundColor(toggle ? .white : .black)
-                
+                // this view is for the user to put his name in it will save the entered name in name variable
                 EnterNameView(name: $name)
                     .padding()
                 
+                // this button updates the name shown in the screen to the entered name
                 Button(action: {
                     self.displayedName = self.name
                 }) {
@@ -43,10 +51,8 @@ struct ProfileView: View {
                 .background(toggle ? Color.white : Color.gray)
                 .cornerRadius(10)
                 .shadow(radius: 5)
-                
-                
                 Spacer()
-                
+                Spacer()
             }
         }
     }
