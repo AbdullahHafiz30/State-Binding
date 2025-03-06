f1f3671bb454291e7c22977837c8b4198538d142
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-06 12:15:48 +0300

    Initial Commit

---
+++ State-Binding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  State-Binding
+//
+//  Created by عبدالله حافظ on 06/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ State-Binding/State_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_BindingApp.swift
+//  State-Binding
+//
+//  Created by عبدالله حافظ on 06/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct State_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
